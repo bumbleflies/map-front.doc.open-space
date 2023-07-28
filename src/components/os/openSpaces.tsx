3fafb9bf42fcb93dd031e1d3e3122064b8f295fc
dachrisch <christian.daehn@gmail.com>
2023-07-28 03:42:04 +0200

    Line 1:8:  'React' is defined but never used  @typescript-eslint/no-unused-vars

--- src/components/os/openSpaces.tsx
+++ src/components/os/openSpaces.tsx
@@ -1,4 +1,4 @@
-import React, {createContext} from 'react';
+import {createContext} from 'react';
 import {OsImageNotAvailable, OsImageType} from "../../types/api";
 
 export type  OpenSpaceImagesContextType = {
