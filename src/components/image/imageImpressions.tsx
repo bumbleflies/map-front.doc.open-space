0f9130b30ed4d5806cd33486d7881ef32e45e82d
dachrisch <christian.daehn@gmail.com>
2023-08-15 00:51:34 +0200

    tab for impressions and sessions

---
+++ src/components/image/imageImpressions.tsx
@@ -0,0 +1,89 @@
+import {IconButton, ImageList, ImageListItem, ImageListItemBar, ListItemButton, Skeleton} from "@mui/material";
+import React, {useEffect, useState} from "react";
+import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
+import {useFetcher, useLoaderData} from "react-router-dom";
+import {Endpoints} from "../../config/Endpoints";
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import {ImageWithDetailsType} from "../../types/image";
+import {OpenSpaceImageAddDialog} from "./osImageAddDialog";
+import {OsImageMenu} from "../menu/osImageMenu";
+
+export const OsImageImpressionsList = () => {
+    const images = useLoaderData() as ImageWithDetailsType[]
+    const [selectedImage, setSelectedImage] = useState<string | null>(null);
+    const [menuAnchorEl, setMenuAnchorEl] = useState<null | HTMLElement>(null);
+    const [editOpen, setEditOpen] = useState<boolean>(false)
+    const [pendingImages, setPendingImages] = useState<string[]>([])
+
+    const imageUploadFetcher = useFetcher()
+
+    useEffect(() => {
+        if (Boolean(imageUploadFetcher.data)) {
+            console.log('make skeletons for pending images: ' + JSON.stringify(imageUploadFetcher.data))
+            setPendingImages(imageUploadFetcher.data)
+        }
+    }, [imageUploadFetcher.data, setPendingImages])
+
+    const openMenu = (event: React.MouseEvent<HTMLButtonElement>, imageIdentifier: string) => {
+        setMenuAnchorEl(event.currentTarget);
+        setSelectedImage(imageIdentifier)
+    };
+
+    const closeMenu = () => {
+        setMenuAnchorEl(null);
+        setSelectedImage(null)
+    }
+
+    return (
+        <>
+            <ImageList>
+                <ImageListItem key={"image-add"} sx={{
+                    alignItems: "center",
+                    verticalAlign: "middle",
+                }}>
+                    <ListItemButton data-testid={"os-image-add-button"} onClick={() => setEditOpen(true)} sx={{
+                        minHeight: 150
+                    }}>
+                        <AddPhotoAlternateIcon fontSize={"large"}/>
+                    </ListItemButton>
+                    <OpenSpaceImageAddDialog isOpen={editOpen} closeHandler={() => setEditOpen(false)}
+                                             submit={imageUploadFetcher.submit}/>
+                    <ImageListItemBar {...images.length === 0 ? {title: "no images yet"} : null}
+                                      subtitle={"click to add impressions"}/>
+
+                </ImageListItem>
+                {images.map((image) => (
+                    <ImageListItem key={image.imageIdentifier}>
+                        <img
+                            src={Endpoints.openSpaceImage(image)}
+                            alt={image.imageIdentifier}
+                            loading="lazy"
+                        />
+                        <ImageListItemBar
+                            title={image.description}
+                            subtitle={image.imageIdentifier}
+                            actionIcon={
+                                <IconButton
+                                    data-testid={"os-image-menu"}
+                                    sx={{color: 'white'}}
+                                    aria-label={`Open Space impression ${image.description}`}
+                                    onClick={(event: React.MouseEvent<HTMLButtonElement>) => openMenu(event, image.imageIdentifier)}
+                                >
+                                    <KeyboardArrowUpIcon/>
+                                </IconButton>
+                            }
+                        />
+                    </ImageListItem>
+                ))}
+                {pendingImages.map((image) => (
+                    <ImageListItem key={image}>
+                        <Skeleton variant="rectangular" width={210} height={150}/>
+                        <ImageListItemBar title={image}
+                        />
+                    </ImageListItem>
+                ))}
+            </ImageList>
+            <OsImageMenu anchorElement={menuAnchorEl} imageId={selectedImage} closeMenuHandler={closeMenu}/>
+        </>
+    )
+}
--- src/components/image/osImages.tsx
+++ src/components/image/osImages.tsx
@@ -1,112 +1,38 @@
-import {
-    IconButton,
-    ImageList,
-    ImageListItem,
-    ImageListItemBar,
-    ListItem,
-    ListItemButton,
-    ListSubheader,
-    Skeleton
-} from "@mui/material";
-import React, {useEffect, useState} from "react";
-import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
-import {useFetcher, useLoaderData, useNavigate, useParams} from "react-router-dom";
-import {Endpoints} from "../../config/Endpoints";
+import {ButtonBase, Tab} from "@mui/material";
+import React, {useState} from "react";
+import {useNavigate} from "react-router-dom";
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-import {ImageWithDetailsType} from "../../types/image";
-import {OpenSpaceImageAddDialog} from "./osImageAddDialog";
-import {OsImageMenu} from "../menu/osImageMenu";
+import {TabContext, TabList, TabPanel} from "@mui/lab";
+import {OsImageImpressionsList} from "./imageImpressions";
 
 export const OpenSpaceImages = () => {
     const navigate = useNavigate()
-    const images = useLoaderData() as ImageWithDetailsType[]
-    const {os_id} = useParams<"os_id">();
-    const [menuAnchorEl, setMenuAnchorEl] = useState<null | HTMLElement>(null);
-    const [selectedImage, setSelectedImage] = useState<string | null>(null);
-    const imageUploadFetcher = useFetcher()
+    const [selectedTab, setSelectedTab] = useState<string>("1")
 
-    const [editOpen, setEditOpen] = useState<boolean>(false)
-    const [pendingImages, setPendingImages] = useState<string[]>([])
 
-    useEffect(() => {
-        if (Boolean(imageUploadFetcher.data)) {
-            console.log('make skeletons for pending images: ' + JSON.stringify(imageUploadFetcher.data))
-            setPendingImages(imageUploadFetcher.data)
-        }
-    }, [imageUploadFetcher.data, setPendingImages])
-
-    const openMenu = (event: React.MouseEvent<HTMLButtonElement>, imageIdentifier: string) => {
-        setMenuAnchorEl(event.currentTarget);
-        setSelectedImage(imageIdentifier)
+    const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+        setSelectedTab(newValue);
     };
-
-    const closeMenu = () => {
-        setMenuAnchorEl(null);
-        setSelectedImage(null)
-    }
 
 
     return (
         <>
-            <ImageList>
-                <ListItem key="Subheader">
-                    <ListItemButton data-testid={"os-images-back-button"} aria-label={"back"} onClick={() => {
-                        navigate(`/os/${os_id}`)
+            <TabContext value={selectedTab}>
+                <TabList onChange={handleChange}>
+                    <ButtonBase data-testid={"os-images-back-button"} aria-label={"back"} onClick={() => {
+                        navigate('..')
                     }}>
                         <ArrowBackIcon/>
-                    </ListItemButton>
-                    <ListSubheader component="div">Impressions</ListSubheader>
-                </ListItem>
-                <ListItem>
-                </ListItem>
-                <ImageListItem key={"image-add"} sx={{
-                    alignItems: "center",
-                    verticalAlign: "middle",
-                }}>
-                    <ListItemButton data-testid={"os-image-add-button"} onClick={() => setEditOpen(true)} sx={{
-                        minHeight: 150
-                    }}>
-                        <AddPhotoAlternateIcon fontSize={"large"}/>
-                    </ListItemButton>
-                    <OpenSpaceImageAddDialog isOpen={editOpen} closeHandler={() => setEditOpen(false)}
-                                             submit={imageUploadFetcher.submit}/>
-                    <ImageListItemBar {...images.length === 0 ? {title: "no images yet"} : null}
-                                      subtitle={"click to add impressions"}/>
+                    </ButtonBase>
+                    <Tab label="Impressions" value={"1"}></Tab>
+                    <Tab label="Sessions" value={"2"}></Tab>
+                </TabList>
+                <TabPanel value={"1"}>
+                    <OsImageImpressionsList/>
+                </TabPanel>
+                <TabPanel value={"2"}>Sessions</TabPanel>
+            </TabContext>
 
-                </ImageListItem>
-                {images.map((image) => (
-                    <ImageListItem key={image.imageIdentifier}>
-                        <img
-                            src={Endpoints.openSpaceImage(image)}
-                            alt={image.imageIdentifier}
-                            loading="lazy"
-                        />
-                        <ImageListItemBar
-                            title={image.description}
-                            subtitle={image.imageIdentifier}
-                            actionIcon={
-                                <IconButton
-                                    data-testid={"os-image-menu"}
-                                    sx={{color: 'white'}}
-                                    aria-label={`Open Space impression ${image.description}`}
-                                    onClick={(event: React.MouseEvent<HTMLButtonElement>) => openMenu(event, image.imageIdentifier)}
-                                >
-                                    <KeyboardArrowUpIcon/>
-                                </IconButton>
-                            }
-                        />
-                    </ImageListItem>
-                ))}
-                {pendingImages.map((image) => (
-                    <ImageListItem key={image}>
-                        <Skeleton variant="rectangular" width={210} height={150}/>
-                        <ImageListItemBar title={image}
-                        />
-                    </ImageListItem>
-                ))}
-            </ImageList>
-            <OsImageMenu anchorElement={menuAnchorEl} imageId={selectedImage} closeMenuHandler={closeMenu}/>
         </>
     )
 }
