5a2ce41ad303e4f35cbc1e83d46370b2f0ece927
dachrisch <christian.daehn@gmail.com>
2023-09-07 14:49:10 +0200

    Refactor screen size test to iterate through different viewports

--- cypress/e2e/screenSizes.cy.ts
+++ cypress/e2e/screenSizes.cy.ts
@@ -1,5 +1,7 @@
 import ViewportPreset = Cypress.ViewportPreset;
 
+import screenSizes from '../fixtures/screens.json'
+
 describe('clicks through on different viewports', () => {
     before(() => {
         cy.registerInterceptRoutes()
@@ -10,41 +12,36 @@ describe('clicks through on different viewports', () => {
         cy.wait('@googlemaps')
     })
 
-    it('should do the same on every screen', () => {
-
-        cy.fixture('screens').then((viewports: ViewportPreset[]) => {
-            viewports.forEach((viewport) => {
-                describe(`runs in ${viewport} view`, () => {
-                    cy.viewport(viewport)
-                    cy.clickAddOs()
-                    cy.clickStatusMessage()
-                    cy.getByDataTestId("os-title").should("contain.text", "Open Space @")
-                    cy.getByDataTestId('grid-identifier-text').then((osIdElement) => {
-                        cy.openEditAssertTitle(osIdElement.text(), 'Open Space')
-                    })
-                    cy.assertNoImages()
-
-                    cy.clickImagesView()
-                    cy.uploadImage('cypress/fixtures/test-image.png')
-
-                    cy.getByDataTestId('os-image').click()
-                    cy.getByDataTestId('os-image-fullscreen').should('exist')
-                    cy.getByDataTestId('os-image-fullscreen-close-button').click()
-                    cy.clickImagesBack()
-
-
-                    cy.getByDataTestId("os-images-button").click()
-                    cy.getByDataTestId('os-image-menu').click()
-                    cy.getByDataTestId('os-image-delete-menu').click()
-                    cy.get('div.MuiImageListItemBar-title').contains('no images yet').should('exist')
-                    cy.clickImagesBack()
-
-                    cy.clickDeleteOs()
-                })
+    screenSizes.forEach(screenSize  => {
+        it(`runs on ${screenSize}`, () => {
+            cy.viewport(screenSize as ViewportPreset)
+            cy.clickAddOs()
+            cy.clickStatusMessage()
+            cy.getByDataTestId("os-title").should("contain.text", "Open Space @")
+            cy.getByDataTestId('grid-identifier-text').then((osIdElement) => {
+                cy.openEditAssertTitle(osIdElement.text(), 'Open Space')
             })
+            cy.assertNoImages()
 
+            cy.clickImagesView()
+            cy.uploadImage('cypress/fixtures/test-image.png')
+
+            cy.getByDataTestId('os-image').click()
+            cy.getByDataTestId('os-image-fullscreen').should('exist')
+            cy.getByDataTestId('os-image-fullscreen-close-button').click()
+            cy.clickImagesBack()
+
+
+            cy.getByDataTestId("os-images-button").click()
+            cy.getByDataTestId('os-image-menu').click()
+            cy.getByDataTestId('os-image-delete-menu').click()
+            cy.get('div.MuiImageListItemBar-title').contains('no images yet').should('exist')
+            cy.clickImagesBack()
+
+            cy.clickDeleteOs()
         })
     })
+
 })
 
 export {}
