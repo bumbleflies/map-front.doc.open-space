c12d3511e3e9a053dc2e759d4b4b5b491a97ea21
dachrisch <christian.daehn@gmail.com>
2023-09-09 02:40:58 +0200

    Add drag and drop styles to image upload component

--- src/components/image/imageUpload.tsx
+++ src/components/image/imageUpload.tsx
@@ -1,22 +1,61 @@
 import {Container, FilePreview, img, thumb, thumbInner, thumbsContainer} from "./filePreview";
 import {useDropzone} from "react-dropzone";
-import React, {useEffect} from "react";
+import React, {useEffect, useMemo} from "react";
 import {Box} from "@mui/material";
 
 type ImageUploadProps = {
     files: FilePreview[],
     onSelectHandler: (acceptedFiles: File[]) => void
 }
+
+const baseStyle = {
+    flex: 1,
+    display: 'flex',
+    alignItems: 'center',
+    padding: '20px',
+    borderWidth: 2,
+    borderRadius: 2,
+    borderColor: '#eeeeee',
+    borderStyle: 'dashed',
+    backgroundColor: '#fafafa',
+    color: '#bdbdbd',
+    outline: 'none',
+    transition: 'border .24s ease-in-out'
+};
+
+const focusedStyle = {
+    borderColor: '#2196f3'
+};
+
+const acceptStyle = {
+    borderColor: '#00e676'
+};
+
+const rejectStyle = {
+    borderColor: '#ff1744'
+};
 export const ImageUpload = (props: ImageUploadProps) => {
     const {
         getRootProps,
         getInputProps,
+        isFocused,
+        isDragAccept,
+        isDragReject,
     } = useDropzone({
             accept: {'image/*': []},
             onDrop: props.onSelectHandler,
         }
     );
-
+    const style = useMemo(() => ({
+        ...baseStyle,
+        ...(isFocused ? focusedStyle : {}),
+        ...(isDragAccept ? acceptStyle : {}),
+        ...(isDragReject ? rejectStyle : {})
+    }), [
+        isFocused,
+        isDragAccept,
+        isDragReject
+    ]);
 
     useEffect(() => {
         // Make sure to revoke the data uris to avoid memory leaks, will run on unmount
@@ -40,7 +79,7 @@ export const ImageUpload = (props: ImageUploadProps) => {
     ));
     return (
         <Box>
-            <Container {...getRootProps()}>
+            <Container {...getRootProps({style})}>
                 <input data-testid={"os-image-upload"} {...getInputProps()} />
                 <p>Drag 'n' drop some files here, or click to select files</p>
             </Container>
