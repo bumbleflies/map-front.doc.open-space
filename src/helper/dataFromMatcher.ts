8722d71cd843219e901999a949c98853711bb3b1
dachrisch <christian.daehn@gmail.com>
2023-09-13 13:14:00 +0200

    added to dependency property

--- src/helper/dataFromMatcher.ts
+++ src/helper/dataFromMatcher.ts
@@ -2,21 +2,20 @@ import {useMatches} from "react-router-dom";
 import {Dispatch, SetStateAction, useEffect} from "react";
 
 type DataFromMatcherProps<T> = {
-    id:string,
+    id: string,
     stateSetter: Dispatch<SetStateAction<T>>
 }
-export const useDataFromMatcher = <T>({id,stateSetter}: DataFromMatcherProps<T>): T => {
+export const useDataFromMatcher = <T>({id, stateSetter}: DataFromMatcherProps<T>): T => {
     const matches = useMatches()
 
-    function getMatchedData() {
-        return matches.find(m => m.id === id)!.data as T;
-    }
+    const getMatchedData = () => matches.find(m => m.id === id)!.data as T;
+
 
     useEffect(() => {
         const matchedData = getMatchedData()
         console.log(`Loading data from matches: ${JSON.stringify(matches)}: ${JSON.stringify(matchedData)}`)
         stateSetter(matchedData)
-    }, [matches, stateSetter])
+    }, [matches, stateSetter, getMatchedData])
 
     return getMatchedData()
 }
