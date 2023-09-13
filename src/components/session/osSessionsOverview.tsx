f7657f700eea6dd443e23b2d8ef1f009cfa74c93
dachrisch <christian.daehn@gmail.com>
2023-09-13 05:49:02 +0200

    moved await into inside of image list

--- src/components/session/osSessionsOverview.tsx
+++ src/components/session/osSessionsOverview.tsx
@@ -1,4 +1,4 @@
-import {IconButton, ImageList, ImageListItem, ImageListItemBar, ListItemButton, Typography} from "@mui/material"
+import {IconButton, ImageList, ImageListItem, ImageListItemBar, ListItemButton} from "@mui/material"
 import NoteAddIcon from '@mui/icons-material/NoteAdd';
 import {Await, useFetcher, useLoaderData, useNavigate} from "react-router-dom";
 import {OsSessionDetailsApiType, OsSessionImage, OsWithSessions} from "../../types/session";
@@ -39,12 +39,6 @@ export const OsSessionsOverview = () => {
 
     return (
         <>
-            <React.Suspense
-                fallback={<p>Loading Sessions...</p>}
-            >
-                <Await
-                    resolve={deferredData.osWithSession}>
-                    {(osWithSessionData) => (
             <ImageList>
                 <ImageListItem key={"session-add"} sx={{
                     alignItems: "center",
@@ -57,57 +51,58 @@ export const OsSessionsOverview = () => {
                                     }}>
                         <NoteAddIcon fontSize={"large"}/>
                     </ListItemButton>
-                    {osWithSessions!==null?
-                    <ImageListItemBar title={osWithSessions.sessions.length > 0 ? null : "no sessions yet"}
-                                      subtitle={"click to add impressions"}/>
+                    {osWithSessions !== null ?
+                        <ImageListItemBar title={osWithSessions.sessions.length > 0 ? null : "no sessions yet"}
+                                          subtitle={"click to add impressions"}/>
                         : null}
                 </ImageListItem>
-                {osWithSessions===null? null : osWithSessions.sessions.map((session) => (
-                    <ImageListItem key={session.sessionIdentifier}>
-                        {session.header.isAvailable ?
-                            <img onClick={() => navigate(`${session.sessionIdentifier}/i`)}
-                                 src={Endpoints.openSpaceSessionImage((session.header as OsSessionImage))}
-                                 alt={session.sessionIdentifier}
-                                 loading="lazy"
-                                 data-testid={"os-session"}
-                            /> :
-                            <img
-                                src={'/img/no-image-icon.png'}
-                                onClick={() => navigate(`${session.sessionIdentifier}/i`)}
-                                alt={'not available yet'}
-                                data-testid={"os-session"}
-                            />
-                        }
+                <React.Suspense
+                    fallback={<p>Loading Sessions...</p>}
+                >
+                    <Await
+                        resolve={deferredData.osWithSession}>
+                        {(osWithSessionData: OsWithSessions) => (
+                            osWithSessionData.sessions.map((session) => (
+                                <ImageListItem key={session.sessionIdentifier}>
+                                    {session.header.isAvailable ?
+                                        <img onClick={() => navigate(`${session.sessionIdentifier}/i`)}
+                                             src={Endpoints.openSpaceSessionImage((session.header as OsSessionImage))}
+                                             alt={session.sessionIdentifier}
+                                             loading="lazy"
+                                             data-testid={"os-session"}
+                                        /> :
+                                        <img
+                                            src={'/img/no-image-icon.png'}
+                                            onClick={() => navigate(`${session.sessionIdentifier}/i`)}
+                                            alt={'not available yet'}
+                                            data-testid={"os-session"}
+                                        />
+                                    }
 
-                        <ImageListItemBar
-                            data-testid={'os-session-time-bar'}
-                            subtitle={`${session.startDate.format('DD.MM')} ${session.startDate.format('HH:mm')} - ${session.endDate.format('HH:mm')}`}
-                            actionIcon={
-                                <IconButton
-                                    data-testid={"os-session-menu"}
-                                    sx={{color: 'white'}}
-                                    aria-label={`Session ${session.title}`}
-                                    onClick={(event: React.MouseEvent<HTMLButtonElement>) => menu.open(event, session.sessionIdentifier)}
-                                >
-                                    <KeyboardArrowUpIcon/>
-                                </IconButton>
-                            }/>
-                        <ImageListItemBar
-                            position={"top"}
-                            actionPosition={"left"}
-                            subtitle={session.title}
-                            data-testid={'os-session-title-bar'}/>
+                                    <ImageListItemBar
+                                        data-testid={'os-session-time-bar'}
+                                        subtitle={`${session.startDate.format('DD.MM')} ${session.startDate.format('HH:mm')} - ${session.endDate.format('HH:mm')}`}
+                                        actionIcon={
+                                            <IconButton
+                                                data-testid={"os-session-menu"}
+                                                sx={{color: 'white'}}
+                                                aria-label={`Session ${session.title}`}
+                                                onClick={(event: React.MouseEvent<HTMLButtonElement>) => menu.open(event, session.sessionIdentifier)}
+                                            >
+                                                <KeyboardArrowUpIcon/>
+                                            </IconButton>
+                                        }/>
+                                    <ImageListItemBar
+                                        position={"top"}
+                                        actionPosition={"left"}
+                                        subtitle={session.title}
+                                        data-testid={'os-session-title-bar'}/>
 
-                    </ImageListItem>
-                ))}
+                                </ImageListItem>
+                            )))}
+                    </Await>
+                </React.Suspense>
             </ImageList>
-            )
-
-                    }
-
-                </Await>
-            </React.Suspense>
-
             <OsSessionsMenu menu={menu} selected={selected}/>
         </>
     )
