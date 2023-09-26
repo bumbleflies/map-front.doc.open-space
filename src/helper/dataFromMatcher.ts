ae356ecfa28250863b12a7ea09395c319fba2fd9
dachrisch <christian.daehn@gmail.com>
2023-09-27 01:44:21 +0200

    Refactor useDataFromMatcher to use useRouteLoaderData instead of useMatches and useState

--- src/helper/dataFromMatcher.ts
+++ src/helper/dataFromMatcher.ts
@@ -1,21 +1,9 @@
-import {useMatches} from "react-router-dom";
-import {useEffect, useState} from "react";
+import {useRouteLoaderData} from "react-router-dom";
 
 type DataFromMatcherProps = {
     id: string,
 }
 
-export const useDataFromMatcher = <T>({id}: DataFromMatcherProps): T|null => {
-    const matches = useMatches()
-
-    const [matched,setMatched]=useState<T|null>(null)
-
-    useEffect(() => {
-        const matchedData = matches.find(m => m.id === id)!.data as T;
-        console.log(`Loading data from matches: ${JSON.stringify(matchedData)}`)
-        setMatched(matchedData)
-    }, [matches, id])
-
-    return matched
-
+export const useDataFromMatcher = <T>({id}: DataFromMatcherProps): T | null => {
+    return useRouteLoaderData(id) as T
 }
